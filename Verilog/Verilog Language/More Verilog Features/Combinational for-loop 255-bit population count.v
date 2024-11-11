6d0657c209542a930448870ebcd18ddec6547800
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:35:11 +0530

    Update Combinational for-loop 255-bit population count.v

--- Verilog/Verilog Language/More Verilog Features/Combinational for-loop 255-bit population count.v
+++ Verilog/Verilog Language/More Verilog Features/Combinational for-loop 255-bit population count.v
@@ -1,8 +1,13 @@
 module top_module( 
-    input [7:0] in,
-    output [7:0] out
-);
-    assign out={in[0],in[1],in[2],in[3],in[4],in[5],in[6],in[7]};
-  
+    input [254:0] in,
+    output [7:0] out );
+    int i ;
     
+    always @(*) begin 
+        out=0;
+    for(i=0; i<255; i++) 
+        if(in[i])begin
+            out=out+1; end
+    end 
+
 endmodule
