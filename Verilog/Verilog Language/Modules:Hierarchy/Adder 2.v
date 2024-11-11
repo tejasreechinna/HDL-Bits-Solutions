6b852e2b4f2ae7ac4ee2c00bef7c332acffa3dc7
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:18:06 +0530

    Update Adder 2.v

--- Verilog/Verilog Language/Modules:Hierarchy/Adder 2.v
+++ Verilog/Verilog Language/Modules:Hierarchy/Adder 2.v
@@ -1,17 +1,17 @@
-module top_module( 
-    input [2:0] a,
-    input [2:0] b,
-    output [2:0] out_or_bitwise,
-    output out_or_logical,
-    output [5:0] out_not
-);
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);//
+    //int cin=1'b0;
+    wire w1, cin , cout1 ;
     
-    assign out_or_bitwise = a | b;
+    add16 g1(.a(a[15:0]) ,.b(b[15:0]) , .cin(0) ,.sum(sum[15:0]) ,.cout( w1));
+    add16 g2(.a(a[31:16]),.b(b[31:16]) , .cin(w1), .sum(sum[31:16]) , .cout(cout1));
 
-   
-     assign   out_or_logical = a||b;
-  
-    assign out_not[2:0]=~a;
-    assign out_not[5:3]=~b;
 endmodule
 
+module add1( input a, input b, input cin,   output sum, output cout );
+assign sum=a^b^cin;
+    assign cout= (a&b)|(cin&(a^b));
+endmodule
