968a686024ab788a3d0cbbf01cc95353d38f0788
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:25:50 +0530

    Update Always blocks (clocked).v

--- Verilog/Verilog Language/Procedures/Always blocks (clocked).v
+++ Verilog/Verilog Language/Procedures/Always blocks (clocked).v
@@ -1,15 +1,11 @@
 module top_module(
-    input [31:0] a,
-    input [31:0] b,
-    input sub,
-    output [31:0] sum
-);
- wire w1,w2;
-    reg [31:0] b_in;
-    assign b_in={32{sub}}^b;
-    //wire [15:0] sum1;
-    add16 g1(a[15:0] ,b_in[15:0],sub ,sum[15:0] , w1);
-    add16 g2(a[31:16], b_in[31:16] , w1 , sum[31:16] , w2);
-                                //assign sum[31:16]=sum[15:0];
-                                
+    input clk,
+    input a,
+    input b,
+    output wire out_assign,
+    output reg out_always_comb,
+    output reg out_always_ff   );
+    assign out_assign = a^b;
+    always @(*) out_always_comb = a^b;
+    always @(posedge clk) out_always_ff = a^b;
 endmodule
