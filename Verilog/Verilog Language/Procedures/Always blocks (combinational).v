7cd83941c5a39024531f5c99c67d7b33ea99b5ab
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:25:25 +0530

    Update Always blocks (combinational).v

--- Verilog/Verilog Language/Procedures/Always blocks (combinational).v
+++ Verilog/Verilog Language/Procedures/Always blocks (combinational).v
@@ -1,16 +1,9 @@
-module top_module( 
-    input [2:0] a,
-    input [2:0] b,
-    output [2:0] out_or_bitwise,
-    output out_or_logical,
-    output [5:0] out_not
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
 );
-    
-    assign out_or_bitwise = a | b;
-
-   
-     assign   out_or_logical = a||b;
-  
-    assign out_not[2:0]=~a;
-    assign out_not[5:3]=~b;
+ assign out_assign = a&b;
+    always @(*) out_alwaysblock =a&b;
 endmodule
