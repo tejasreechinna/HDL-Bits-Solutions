18d904f8d2234649682c61505939e941ca8707d6
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:20:24 +0530

    Update Carry-select adder.v

--- Verilog/Verilog Language/Modules:Hierarchy/Carry-select adder.v
+++ Verilog/Verilog Language/Modules:Hierarchy/Carry-select adder.v
@@ -1,6 +1,15 @@
-module top_module (
-    input a, b, c, d, e,
-    output [24:0] out );
-    assign out = ~{{{5{a}},{5{b}},{5{c}},{5{d}},{5{e}}} ^ {5{a,b,c,d,e}}};
-
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire w1 ;
+    wire [31:0] b_exor;
+    assign b_exor={32{1'b0}}^b;
+    wire [15:0] sum1 , sum2;
+    add16 g1 (a[15:0] , b_exor[15:0] ,0 , sum[15:0] , w1);
+    add16 g2 (a[31:16] , b_exor[31:16], 0 , sum1 );
+    add16 g3(a[31:16] , b_exor[31:16] , 1 , sum2 );
+    assign sum[31:16]=(w1)?sum2:sum1;
+             
 endmodule
