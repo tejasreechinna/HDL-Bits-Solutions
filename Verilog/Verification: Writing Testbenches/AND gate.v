84b44d432b2574ea8946cf0c039daed1cecab220
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:43:36 +0530

    Update AND gate.v

--- Verilog/Verification: Writing Testbenches/AND gate.v
+++ Verilog/Verification: Writing Testbenches/AND gate.v
@@ -1,16 +1,11 @@
-module top_module( 
-    input [2:0] a,
-    input [2:0] b,
-    output [2:0] out_or_bitwise,
-    output out_or_logical,
-    output [5:0] out_not
-);
-    
-    assign out_or_bitwise = a | b;
-
-   
-     assign   out_or_logical = a||b;
-  
-    assign out_not[2:0]=~a;
-    assign out_not[5:3]=~b;
-endmodule
+module top_module();
+    reg [1:0] in ;
+    wire out;
+    andgate dut(.in(in) , .out(out));
+    initial begin 
+        in=2'b00; #10;
+        in=2'b01; #10;
+        in=2'b10;#10;
+        in=2'b11;#10;
+    end
+    endmodule
