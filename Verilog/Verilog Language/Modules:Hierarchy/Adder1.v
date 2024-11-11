6c5bcaecb076d74b192d72a6cbefd348e18342b5
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:18:36 +0530

    Update Adder1.v

--- Verilog/Verilog Language/Modules:Hierarchy/Adder1.v
+++ Verilog/Verilog Language/Modules:Hierarchy/Adder1.v
@@ -1,16 +1,8 @@
-module top_module( 
-    input [2:0] a,
-    input [2:0] b,
-    output [2:0] out_or_bitwise,
-    output out_or_logical,
-    output [5:0] out_not
-);
-    
-    assign out_or_bitwise = a | b;
-
-   
-     assign   out_or_logical = a||b;
-  
-    assign out_not[2:0]=~a;
-    assign out_not[5:3]=~b;
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);wire w1;
+    add16 m1(.a(a[15:0]) ,.b(b[15:0]) , .cin(1'b0) , .sum(sum[15:0]), .cout(w1));
+    add16 m2(.a(a[31:16]) , .b(b[31:16]),.cin( w1) ,.sum(sum[31:16]));
 endmodule
