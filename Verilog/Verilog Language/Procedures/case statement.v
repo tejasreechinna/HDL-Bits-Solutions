9fa7fe3df21b7eb830b7828ca888947a50e3ef66
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:27:17 +0530

    Update case statement.v

--- Verilog/Verilog Language/Procedures/case statement.v
+++ Verilog/Verilog Language/Procedures/case statement.v
@@ -1,8 +1,23 @@
-module top_module( 
-    input [7:0] in,
-    output [7:0] out
-);
-    assign out={in[0],in[1],in[2],in[3],in[4],in[5],in[6],in[7]};
-  
-    
+module top_module ( 
+    input [2:0] sel, 
+    input [3:0] data0,
+    input [3:0] data1,
+    input [3:0] data2,
+    input [3:0] data3,
+    input [3:0] data4,
+    input [3:0] data5,
+    output reg [3:0] out   );//
+
+    always@(*) begin  // This is a combinational circuit
+        case(sel)
+            3'd0:out=data0;
+            3'd1:out=data1;
+            3'd2:out=data2;
+            3'd3:out=data3;
+            3'd4:out=data4;
+            3'd5:out=data5;
+            default : out=4'b0000;
+        endcase
+            end
+
 endmodule
