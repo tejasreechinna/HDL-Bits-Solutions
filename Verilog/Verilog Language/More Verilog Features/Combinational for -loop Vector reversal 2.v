d3c322a147980d223f48223d6dc3d82dcfc0144d
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:34:44 +0530

    Update Combinational for -loop Vector reversal 2.v

--- Verilog/Verilog Language/More Verilog Features/Combinational for -loop Vector reversal 2.v
+++ Verilog/Verilog Language/More Verilog Features/Combinational for -loop Vector reversal 2.v
@@ -1,6 +1,10 @@
-module top_module (
-    input a, b, c, d, e,
-    output [24:0] out );
-    assign out = ~{{{5{a}},{5{b}},{5{c}},{5{d}},{5{e}}} ^ {5{a,b,c,d,e}}};
-
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+); int i;
+    always @(*) begin
+        for(i=0;i<100;i++)
+            out[i]=in[99-i];
+    end 
+     
 endmodule
