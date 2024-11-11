27a4a06a006f938db41d6867e50599f4f4927fdb
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:43:55 +0530

    Update Testbench2.v

--- Verilog/Verification: Writing Testbenches/Testbench2.v
+++ Verilog/Verification: Writing Testbenches/Testbench2.v
@@ -1,6 +1,22 @@
-module top_module (
-    input a, b, c, d, e,
-    output [24:0] out );
-    assign out = ~{{{5{a}},{5{b}},{5{c}},{5{d}},{5{e}}} ^ {5{a,b,c,d,e}}};
+module top_module();
+    reg clk , in ;
+    reg [2:0] s ;
+    wire out;
+    
+    q7 dut(.clk(clk) , .in(in) , .s(s));
+ initial begin 
+     clk=0;
+     forever #5 clk=~clk;
+ end 
+    initial begin 
+        s=3'd2; in=0; #10;
+        s=3'd6 ; in=0; #10;
+        s= 3'd2; in=1; #10;
+        s=3'd7 ; in=0; #10;
+        s=3'd0; in=1; # 30;
+        in=0;s=3'd0;
+            
+    end
+            
 
 endmodule
