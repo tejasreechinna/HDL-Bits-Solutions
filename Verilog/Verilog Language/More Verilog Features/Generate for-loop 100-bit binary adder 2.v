1b3e66a79f4692be210ea258b85eb65c886e6f55
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:35:33 +0530

    Update Generate for-loop 100-bit binary adder 2.v

--- Verilog/Verilog Language/More Verilog Features/Generate for-loop 100-bit binary adder 2.v
+++ Verilog/Verilog Language/More Verilog Features/Generate for-loop 100-bit binary adder 2.v
@@ -1,10 +1,15 @@
-module top_module ( 
-    input a, 
-    input b, 
-    input c,
-    input d,
-    output out1,
-    output out2
-);
-    mod_a dut(out1 , out2 , a , b , c , d);
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output [99:0] cout,
+    output [99:0] sum );
+ 
+    assign {sum} = a+b+cin;
+    assign cout[0]= a[0]&b[0]|cin&(a[0]^b[0]);
+    always @(*)
+        begin 
+            for( int i=1 ; i<100 ; i=i+1 )
+                cout[i]=a[i]&b[i]|cout[i-1]&(a[i]^b[i]);
+        end 
+
 endmodule
