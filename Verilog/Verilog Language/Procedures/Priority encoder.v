a76bde5a888afd97c700b900517d879ff9bd2550
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:27:43 +0530

    Update Priority encoder.v

--- Verilog/Verilog Language/Procedures/Priority encoder.v
+++ Verilog/Verilog Language/Procedures/Priority encoder.v
@@ -1,10 +1,15 @@
-module top_module ( 
-    input a, 
-    input b, 
-    input c,
-    input d,
-    output out1,
-    output out2
-);
-    mod_a dut(out1 , out2 , a , b , c , d);
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input [3:0] in,
+    output reg [1:0] pos  );
+    always @(*)
+        begin 
+            case(in)
+                4'd1,4'd3,4'd5,4'd7,4'd9,4'd11,4'd13,4'd15: pos = 2'd0;
+                4'd2,4'd6,4'd10, 4'd14:pos = 2'd1;
+                4'd4,4'd12:pos = 2'd2;
+                4'd8:pos = 2'd3;
+                default : pos=3'b000;
+            endcase
+        end 
 endmodule
