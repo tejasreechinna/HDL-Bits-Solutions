35c80370119767317db6dd4cfcebbf47a424d8e9
tejasreechinna <136680460+tejasreechinna@users.noreply.github.com>
2024-11-11 14:34:17 +0530

    Update Reduction Even wider gates.v

--- Verilog/Verilog Language/More Verilog Features/Reduction Even wider gates.v
+++ Verilog/Verilog Language/More Verilog Features/Reduction Even wider gates.v
@@ -1,16 +1,11 @@
 module top_module( 
-    input [2:0] a,
-    input [2:0] b,
-    output [2:0] out_or_bitwise,
-    output out_or_logical,
-    output [5:0] out_not
+    input [99:0] in,
+    output out_and,
+    output out_or,
+    output out_xor 
 );
-    
-    assign out_or_bitwise = a | b;
+    assign out_and = & in[99:0];
+    assign out_or = |in[99:0];
+    assign out_xor = ^in[99:0];
 
-   
-     assign   out_or_logical = a||b;
-  
-    assign out_not[2:0]=~a;
-    assign out_not[5:3]=~b;
 endmodule
